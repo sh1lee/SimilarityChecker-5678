5fcb67b0b82f4557828eb8035b54a7a9bf8d929a
sh1.lee <sh1.lee@samsung.com>
2025-07-23 15:35:42 +0900

    [feature] diff length

--- SimilarityChecker/similaritychecker.cpp
+++ SimilarityChecker/similaritychecker.cpp
@@ -7,10 +7,18 @@ public:
     int getResult(string str1, string str2) {
         int s1 = (int)str1.size();
         int s2 = (int)str2.size();
-        if (s1 == s2) {
-            return 60;
+        int result = 0;
+        int gap = 0;
+        if (s1 == s2) return 60;
+        if (s1 < s2) {
+            gap = s2 - s1;
+            result = 1 - (gap / s1);
         }
- 
+        else {
+            gap = s1 - s2;
+            result = 1 - (gap / s2);
+        }
+        return result;
     }
 
 };
--- SimilarityChecker/similaritychecker_test.cpp
+++ SimilarityChecker/similaritychecker_test.cpp
@@ -9,3 +9,9 @@ TEST(TC, Samelength) {
     EXPECT_EQ(expected, result);
 }
 
+TEST(TC, DiffLengthS1Short) {
+    LengthCheck app;
+    int result = app.getResult("A", "BB");
+    int expected = 0;
+    EXPECT_EQ(expected, result);
+}
